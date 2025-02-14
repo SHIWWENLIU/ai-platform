1f9a457f68a0eb87883d7954ad7491bbfd5cfc36
SHIWWENLIU <1033674633@qq.com>
2025-02-14 11:56:25 +1030

    init project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import type { Configuration as WebpackConfig } from 'webpack'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  webpack: (config: WebpackConfig) => {
+    config.ignoreWarnings = [{ module: /node_modules\/punycode/ }]
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
